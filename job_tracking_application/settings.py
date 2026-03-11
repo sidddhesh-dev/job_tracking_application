6a1711e4d58b58d5bd36f605b251ef272227f87b
sidddhesh-dev <siddhesh.s.thorat14@gmail.com>
2026-03-12 02:05:33 +0530

    database issue fixed for deployment

--- job_tracking_application/settings.py
+++ job_tracking_application/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +80,9 @@ WSGI_APPLICATION = 'job_tracking_application.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
